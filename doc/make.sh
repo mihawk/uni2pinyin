a7a41ed9cfe0b6a9d0317c3314d7a6aef4c7f11d
chan <regio_marineris@hotmail.com>
2014-07-23 17:50:24 +0800

    init repo

---
+++ doc/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+zcat Uni2Pinyin | grep "^[0-9]" | awk '
+BEGIN {print "-module(uni2pinyin).\n-export([decode/1]).\n"} 
+{if(NF>2) {printf "decode(16#"$1") -> [ "; for(i=2;i<NF;i++){printf "\""$i"\","}; printf "\""$NF"\" ];\n"} else 
+{print "decode(16#"$1") -> \""$2"\";"}} 
+END   {print "decode(_) -> {error, unknow}."}
+'  > ../src/uni2pinyin.erl
+
+
