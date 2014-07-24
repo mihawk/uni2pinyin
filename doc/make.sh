cc9772e049e0b50f618d70d2bf7841be625cfa46
chan <chan.sisowath@gmail.com>
2014-07-24 23:19:34 +0800

    be sure that src existe

--- doc/make.sh
+++ doc/make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir ../src
 zcat Uni2Pinyin.gz | grep "^[0-9]" | awk '
 BEGIN {print "-module(uni2pinyin).\n-export([decode/1]).\n"} 
 {if(NF>2) {printf "decode(16#"$1") -> [ "; for(i=2;i<NF;i++){printf "\""$i"\","}; printf "\""$NF"\" ];\n"} else 
