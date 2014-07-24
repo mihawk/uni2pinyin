a5cccf81dc40a504c16ec42650d70272938f4698
chan <chan.sisowath@gmail.com>
2014-07-24 23:18:50 +0800

    fix script

--- doc/make.sh
+++ doc/make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-zcat Uni2Pinyin | grep "^[0-9]" | awk '
+zcat Uni2Pinyin.gz | grep "^[0-9]" | awk '
 BEGIN {print "-module(uni2pinyin).\n-export([decode/1]).\n"} 
 {if(NF>2) {printf "decode(16#"$1") -> [ "; for(i=2;i<NF;i++){printf "\""$i"\","}; printf "\""$NF"\" ];\n"} else 
 {print "decode(16#"$1") -> \""$2"\";"}} 
